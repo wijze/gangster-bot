e539994f6bf9f606d3aaf6b2197d20e6a5305aa3
wijze <pieterhelleman@gmail.com>
2023-01-10 15:44:41 +0100

    improved chatbot >improved structure >added comments what to do

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,20 @@
 running = True
 
+#shouldn't start at run but rather first ask you what to do and if you answer start then start
 while running:
-    inpString = input("Enter a number or exit to exit: ")
-    if inpString.isdigit():
-        print(int(inpString) + 1)
+    inpString = input("Enter a move or exit to exit: ")
+    #we should implement a more command like help and start
+    if verifyInpStr(inpString):
+        print(answer())
         continue
     elif inpString == "exit":
         running = False
         print("exited")
+
+def verifyInpStr(input):
+    #should implement a way to check if the input is valid
+    return True
+
+def answer():
+    #should generate the answer (the best move)
+    return "answer"
