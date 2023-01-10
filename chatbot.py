e0ffd9f06970b83a065fdca3d2e19621f8769876
wijze <pieterhelleman@gmail.com>
2023-01-10 15:57:37 +0100

    Fixed chatbot.py >fixed a bug were it would't work >added an additional comment

--- chatbot.py
+++ chatbot.py
@@ -3,13 +3,14 @@ running = True
 #shouldn't start at run but rather first ask you what to do and if you answer start then start
 while running:
     inpString = input("Enter a move or exit to exit: ")
-    #we should implement a more command like help and start
-    if verifyInpStr(inpString):
-        print(answer())
-        continue
-    elif inpString == "exit":
+    #we should implement a more commands like help and start
+    #we should use a switch thing  for all possible kinds of answers
+    if inpString == "exit":
         running = False
         print("exited")
+    elif verifyInpStr(inpString):
+        print(answer())
+        continue
 
 def verifyInpStr(input):
     #should implement a way to check if the input is valid
