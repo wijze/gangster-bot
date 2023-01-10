eecc0220663a34b979cceb147828fd5f16730704
GuitarPickle <dan731600@gmail.com>
2023-01-10 21:03:38 +0100

    Update validator.py
    I made a function that makes the previous code faster door het te recompilen.
    
    Ook returned het een move als je een legale move inzet

--- validator.py
+++ validator.py
@@ -1,5 +1,6 @@
 #this file uses python library named re for pattern recognition
 import re
+import random
 #checkStringFormat wants a string and wil return a true if it is in valid chess move format
 
 #I have copied the pattern from https://8bitclassroom.com/2020/08/16/chess-in-regex/
@@ -9,10 +10,20 @@ import re
 #checkStringFormat("1") returns false
 #checkStringFormat("Nbxc5") returns true
 #checkStringFormat("b9") returns false
-def checkStringFormat(toCheck):
-  pattern = r'([Oo0](-[Oo0]){1,2}|[KQRBN]?[a-h]?[1-8]?x?[a-h][1-8](\=[QRBN])?[+#]?(\s(1-0|0-1|1\/2-1\/2))?)'
-  return bool(re.search(pattern, toCheck))
+import re
+import random
 
+def is_valid_move(move_string):
+    move_pattern = r'([Oo0](-[Oo0]){1,2}|[KQRBN]?[a-h]?[1-8]?x?[a-h][1-8](\=[QRBN])?[+#]?(\s(1-0|0-1|1\/2-1\/2))?)'
+    move_regex = re.compile(move_pattern)
+    match = move_regex.findall(move_string)
+    if match:
+        return random.choice(match)
+    else:
+        return False
+
+test_move = "d4"
+print(is_valid_move(test_move))
 #https://8bitclassroom.com/2020/08/16/chess-in-regex/
 #https://stackoverflow.com/questions/9012008/pythons-re-return-true-if-string-contains-regex-pattern
 #https://www.ichess.net/blog/chess-notation/
