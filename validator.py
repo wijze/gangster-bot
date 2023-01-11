f161599eb224a2a3152824dd91d0721e94150fbe
wijze <122311198+wijze@users.noreply.github.com>
2023-01-11 17:33:58 +0100

    updated validator.py
    >removed double imports
    >removed import random totally

--- validator.py
+++ validator.py
@@ -1,6 +1,5 @@
 #this file uses python library named re for pattern recognition
 import re
-import random
 #checkStringFormat wants a string and wil return a true if it is in valid chess move format
 
 #I have copied the pattern from https://8bitclassroom.com/2020/08/16/chess-in-regex/
@@ -10,8 +9,6 @@ import random
 #checkStringFormat("1") returns false
 #checkStringFormat("Nbxc5") returns true
 #checkStringFormat("b9") returns false
-import re
-import random
 
 def checkStringFormat(toCheck):
   move_pattern = r'^([Oo0](-[Oo0]){1,2}|[KQRBN]?[a-h]?[1-8]?x?[a-h][1-8](\=[QRBN])?[+#]?(\s(1-0|0-1|1\/2-1\/2))?)$'
