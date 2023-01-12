9829a74dec2317090679cf93a5ac17dcb389f1bc
wijze <122311198+wijze@users.noreply.github.com>
2023-01-12 09:53:30 +0100

    worked on pon move validation

--- pieces.py
+++ pieces.py
@@ -1,21 +1,22 @@
-#the base class Piece has an init method that defines it own posisition and wheter is is white or not
-#it also has a method IAM that returns it own type
-#and a move method that does nothing for now
+# the base class Piece has an init method that defines it own position and whether is white or not
+# it also has a method IAM that returns it own type
+# and a move method that does nothing for now
 
-#all pieces have their own class that inherits from the Piece class
-#these classes only set their type for now
-#the rook and king class have a special variable for if they have moved for castling
-#the nopiece class has only its position
+# all pieces have their own class that inherits from the Piece class
+# these classes only set their type for now
+# the rook and king class have a special variable for if they have moved for castling
+# the nopiece class has only its position
 
 
-#genMove functionality has to be adde to each piece (exept nopiece)
+# genMove functionality has to be added to each piece (except nopiece)
 
 class Piece:
   def __init__(self, posX, posY, isWhite, board):
     self.posX = posX
     self.posY = posY
     self.isWhite = isWhite
-  
+    self.board = board
+
   def IAM(self):
     return self.type
 
@@ -30,84 +31,101 @@ class Piece:
       "toY": toY
     }
 
+
 class NoPiece(Piece):
   def __init__(self, posX, posY):
-    self.type="NoPiece"
-  
-  def genMoves():
+    self.type = "NoPiece"
+
+  def genMoves(self):
     return []
 
+
 class Pon(Piece):
-  def __init__(self, posX, posY, isWhite):
-    Piece.__init__(posX, posY, isWhite)
-    self.type="Pon"
+  def __init__(self, posX, posY, isWhite, board):
+    Piece.__init__(posX, posY, isWhite, board)
+    self.type = "Pon"
     self.hasMoved = False
-  
-  def genMoves():
+
+  def genMoves(self):
     moves = []
     print("genMoves is not implemented yet")
-    if(self.isWhite):
-      if(not(self.posX >= 7)):
-        if(board.getSquare(self.posX, self.posY+1).type == "NoPiece"):
-          moves.append(self.addMove(self.posX, self.posY+1))
-          #add double step if not has moved
-        #add taking
+    if self.isWhite:
+      if self.posY < 7:
+        if self.board.getSquare(self.posX, self.posY + 1).type == "NoPiece":
+          moves.append(self.addMove(self.posX, self.posY + 1))
+          if not self.hasMoved and self.board.getSquare(self.posX, self.posY + 2).type == "NoPiece" and self.posY==1:
+            moves.append(self.addMove(self.posX, self.posY + 2))
+        if self.board.getSquare(self.posX - 1, self.posY + 1).type != "NoPiece" and not self.board.getSquare(self.posX - 1, self.posY + 1).isWhite:
+          moves.append(self.addMove(self.posX - 1, self.posY + 1))
+        if self.board.getSquare(self.posX + 1, self.posY + 1).type != "NoPiece" and not self.board.getSquare(self.posX + 1, self.posY + 1).isWhite:
+          moves.append(self.addMove(self.posX + 1, self.posY + 1))
+        # add en passant
     else:
-      if(not(self.posX <= 1)):
-        if(board.getSquare(self.posX, self.posY+1).type == "NoPiece"):
-          moves.append(self.addMove(self.posX, self.posY-1))
-          #add double step if not has moved
-        #add taking
+      if self.posY > 0:
+        if self.board.getSquare(self.posX, self.posY + 1).type == "NoPiece":
+          moves.append(self.addMove(self.posX, self.posY - 1))
+          if not self.hasMoved and self.board.getSquare(self.posX, self.posY - 2).type == "NoPiece" and self.posY==6:
+            moves.append(self.addMove(self.posX, self.posY - 2))
+        if self.board.getSquare(self.posX - 1, self.posY - 1).type != "NoPiece" and self.board.getSquare(self.posX - 1, self.posY - 1).isWhite:
+          moves.append(self.addMove(self.posX - 1, self.posY + 1))
+        if self.board.getSquare(self.posX + 1, self.posY - 1).type != "NoPiece" and self.board.getSquare(self.posX + 1, self.posY - 1).isWhite:
+          moves.append(self.addMove(self.posX + 1, self.posY - 1))
+        # add en passant
     return moves
+
 
 class Knight(Piece):
-  def __init__(self, posX, posY, isWhite):
-    Piece.__init__(posX, posY, isWhite)
-    self.type="Knight"
-  
-  def genMoves():
+  def __init__(self, posX, posY, isWhite, board):
+    Piece.__init__(posX, posY, isWhite, board)
+    self.type = "Knight"
+
+  def genMoves(self):
     print("genMoves is not implemented yet")
     moves = []
     return moves
+
 
 class Bishop(Piece):
-  def __init__(self, posX, posY, isWhite):
-    Piece.__init__(posX, posY, isWhite)
-    self.type="Bishop"
-  
-  def genMoves():
+  def __init__(self, posX, posY, isWhite, board):
+    Piece.__init__(posX, posY, isWhite, board)
+    self.type = "Bishop"
+
+  def genMoves(self):
     print("genMoves is not implemented yet")
     moves = []
     return moves
+
 
 class Rook(Piece):
-  def __init__(self, posX, posY, isWhite):
-    Piece.__init__(posX, posY, isWhite)
-    self.type="Rook"
+  def __init__(self, posX, posY, isWhite, board):
+    Piece.__init__(posX, posY, isWhite, board)
+    self.type = "Rook"
     self.hasMoved = False
-  
-  def genMoves():
+
+  def genMoves(self):
     print("genMoves is not implemented yet")
     moves = []
     return moves
+
 
 class Queen(Piece):
-  def __init__(self, posX, posY, isWhite):
-    Piece.__init__(posX, posY, isWhite)
-    self.type="Queen"
-  
-  def genMoves():
+  def __init__(self, posX, posY, isWhite, board):
+    Piece.__init__(posX, posY, isWhite, board)
+    self.type = "Queen"
+
+  def genMoves(self):
     print("genMoves is not implemented yet")
     moves = []
     return moves
 
+
 class King(Piece):
-  def __init__(self, posX, posY, isWhite):
-    Piece.__init__(posX, posY, isWhite)
-    self.type="King"
+  def __init__(self, posX, posY, isWhite, board):
+    Piece.__init__(posX, posY, isWhite, board)
+    self.type = "King"
     self.hasMoved = False
-  
-  def genMoves():
+
+  def genMoves(self):
     print("genMoves is not implemented yet")
     moves = []
     return moves
