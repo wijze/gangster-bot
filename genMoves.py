3bbbe76a1dcb1cbe7beac5ac4dca16ce24a69307
wijze <pieterhelleman@gmail.com>
2023-01-13 23:39:17 +0100

    completed genMoves.py >added functionality for checking if king is in check after move

--- genMoves.py
+++ genMoves.py
@@ -1,13 +1,39 @@
-def genMoves(board):
+def genMoves(board, recursivelyCalled, whiteToPlay):
   moves = []
   #generate all possible moves (not accounting for check)
   for piece in board.array:
-    moves += piece.genMoves()
-  
-  # loop all moves and if it results in the own king being in check delete this move
-  for move in moves:
-    local_board = board.makeMove(move, False)
-    # local_board.testCheck() (to be added)
-    pass
+    if piece.isWhite == whiteToPlay:
+      moves += piece.genMoves()
 
-  return moves
+  # loop all moves and if it results in the own king being in check delete this move
+  for i in range(len(moves)):
+    # check for check by generating all moves of the enemy and checking if they are taking the king
+    # should only do it in depth zero, not in recursive call 
+    if not recursivelyCalled:
+      local_board = board.makeMove(moves[i], False)
+      #checking for check, returns true if in check
+      inCheck = genMoves(local_board, True, not whiteToPlay)
+      if inCheck:
+        #remove this move from the moves
+        moves.pop(i)
+
+    else:
+      #checking for check
+      for x in range(8):
+        for y in range(8):
+          if board.getSquare(x, y).type == "King" and board.getSquare(x, y).isWhite != whiteToPlay:
+            #this is the king of the player to move so it can't be attacked
+            king_pos = [x,y]
+            break
+        if not king_pos:
+          break
+      if not king_pos: print("king not found")
+      else:
+        #check for check by seeing if an enemy piece can take the king (this is in the next turn)
+        if moves[i].toX == king_pos[0] and moves[i].toY == king_pos[1]:
+          #can take the king, delete the original move
+          return True
+  if recursivelyCalled:
+    return False
+  else:
+    return moves
