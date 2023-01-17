946a12ed54267fa4c1ea1a14183f3462f2a69c55
wijze <pieterhelleman@gmail.com>
2023-01-17 22:09:34 +0100

    updated display.py >changed static board image to squares drawn by code >added sources

--- display.py
+++ display.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("test")
@@ -7,8 +6,18 @@ root.title("test")
 quit_button = tk.Button(root, text="Quit", command=root.quit)
 quit_button.pack()
 
-board_img = ImageTk.PhotoImage(Image.open("chessboard.jpg"))
-board_label = tk.Label(image=board_img)
-board_label.pack()
+dimensions = 8
+square_size = 32
+
+colors = ["white", "gray"]
+
+board = tk.Canvas(root, width=dimensions*square_size, height=dimensions*square_size)
+board.pack()
+
+for row in range (dimensions):
+  for column in range (dimensions):
+    if (((row+column)/2)%1) == 0: color = colors[0]
+    else: color = colors[1]
+    board.create_rectangle(column*square_size, row*square_size, (column+1) * square_size, (row+1) * square_size, fill=color, outline="black")
 
 root.mainloop()
