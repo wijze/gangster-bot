b404ac5acb8aab5ee2b3dfdd18e6eaf2b83aa5f3
wijze <122311198+wijze@users.noreply.github.com>
2023-04-07 17:49:04 +0200

    optimizations

--- WithModule/ai.py
+++ WithModule/ai.py
@@ -1,8 +1,7 @@
 import random
 import chess
 
-max_depth = 3
-
+max_depth = 5 # must be bigger then 0
 
 def generate_move(board):
     return search_tree(board, 0, evaluate_position(board))
@@ -16,100 +15,69 @@ values = {
     chess.PAWN: 1
 }
 
-string_to_piece = {
-    'r': 4, # ROOK
-    'n': 2, # KNIGHT
-    'b': 3, # BISHOP
-    'q': 5, # QUEEN
-    'k': 6, # KING
-    'p': 1, # PAWN
-}
-
-
-# def get_all_pieces(board): # because the module is stupid
-#     pieces = []
-#     for i in range(8):
-#         for j in range(8):
-#             piece = board.piece_at(chess.square(i,j))
-#             if piece != None:
-#                 pieces.append(piece)
-#     return pieces
-
 def get_material_balance(board):
     balance = 0 # positive for white advantage
+    piece_map = board.piece_map()
 
-    for i in range(8):
-        for j in range(8):
-            piece = board.piece_at(chess.square(i,j))
-            if piece == None: continue
-
-            if piece.color == chess.WHITE:
-                balance += values[piece.piece_type]
-            else:
-                balance -= values[piece.piece_type]
+    for value in piece_map.values():
+        if value.color == chess.WHITE:
+            balance += values[value.piece_type]
+        else:
+            balance -= values[value.piece_type]
 
     return balance
 
 def evaluate_position(board):
-    # just uses material value as evaluation for now
-
     return get_material_balance(board)
+    # just uses material value as evaluation for now
 
 
 def search_tree(board, depth, layer_up_best_move_evaluation):
     white_to_move = board.turn
 
-    legal_moves = list(board.legal_moves)
-    best_moves = {
-        'evaluation': 0,
-        'move_indexes': []
-    }
+    legal_moves  = list(board.legal_moves)
+    random.shuffle(legal_moves)
 
-    if white_to_move: best_moves['evaluation'] = -100
-    else: best_moves['evaluation'] = 100
+    evaluation_list = []
+    best_move_evaluation = -100 if white_to_move else 100
 
-    if len(legal_moves) == 0:
-        print("warning: no legal moves")
-        print("warning occurred at depth:", depth)
-        return best_moves
+    if depth >= max_depth: 
+        return evaluate_position(board)
 
     for i in range(len(legal_moves)):
         board.push(legal_moves[i])
 
-        if depth >= max_depth:
-            evaluation = evaluate_position(board)
-        else:
-            evaluation = search_tree(board, depth+1, best_moves['evaluation'])['evaluation']
+        evaluation = search_tree(board, depth+1, best_move_evaluation)
+        evaluation_list.append(evaluation)
         
-        if evaluation == best_moves['evaluation']:
-            best_moves['move_indexes'].append(i)
-        elif evaluation > best_moves['evaluation'] and white_to_move:
-            best_moves['evaluation'] = evaluation
-            best_moves['move_indexes'] = [i]
-        elif evaluation < best_moves['evaluation'] and (not white_to_move):
-            best_moves['evaluation'] = evaluation
-            best_moves['move_indexes'] = [i]
+        if evaluation > best_move_evaluation and white_to_move:
+            best_move_evaluation = evaluation
+        elif evaluation < best_move_evaluation and (not white_to_move):
+            best_move_evaluation = evaluation
 
         board.pop()
 
         # alpha-beta pruning optimization
-        if white_to_move and evaluation > layer_up_best_move_evaluation:
+        if white_to_move and evaluation >= layer_up_best_move_evaluation:
             break
-        if (not white_to_move) and evaluation < layer_up_best_move_evaluation:
+        if (not white_to_move) and evaluation <= layer_up_best_move_evaluation:
             break
 
-    chosen_move = legal_moves[best_moves['move_indexes'][random.randrange(0, len(best_moves['move_indexes']))]]
-    move = {
-        'move': chosen_move,
-        'evaluation': best_moves['evaluation']
-    }
-
-    return move
+    if depth == 0:
+        if len(legal_moves)==0:
+            print("error: no legal moves for AI")
+            return
+        move = {
+            'move': legal_moves[list.index(evaluation_list, best_move_evaluation)],
+            'evaluation': best_move_evaluation
+        }
+        return move
+    else: return best_move_evaluation
+    
 
 # to watch performance:
 
 # import cProfile
-# cProfile.run("print(generate_move(chess.Board()))['move']",sort="cumtime")
+# cProfile.run("generate_move(chess.Board())",sort="cumtime")
 
-# 197281 positions evaluated for max_depth = 3 and no alpha-beta (35sec for me)
-# 74781 positions evaluated for max_depth = 3 with alpha-beta (13sec for me)
+# runs in 5 sec with alpha-beta and max_depth=3
--- WithModule/test.py
+++ WithModule/test.py
@@ -1,6 +1,1 @@
-import chess
-
-board = chess.Board()
-piece = board.piece_at(chess.square(1,1))
-
-print(chess.BISHOP)
+print()
