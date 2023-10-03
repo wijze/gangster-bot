fcbb882768f4fd463803a81dd17ab3c1081a0d14
wijze <122311198+wijze@users.noreply.github.com>
2023-10-03 17:30:47 +0200

    improved move ordening, formatted

--- v3/main.py
+++ v3/main.py
@@ -6,6 +6,7 @@ from game import Game
 from multiprocessing import Process, Queue, Pool
 from time import sleep
 
+
 def get_results_formatted(results_array):
     result = ""
     result += "white: " + str(results_array.count("white")) + "\n"
@@ -13,8 +14,16 @@ def get_results_formatted(results_array):
     result += "draw: " + str(results_array.count("draw")) + "\n"
     return result
 
+
 class Settings:
-    def __init__(self, n_games=1, wait_time=0, parralel_simulations=1, print_individual_results=False, print_games_left_every=1) -> None:
+    def __init__(
+        self,
+        n_games=1,
+        wait_time=0,
+        parralel_simulations=1,
+        print_individual_results=False,
+        print_games_left_every=1,
+    ) -> None:
         self.wait_time = wait_time
         self.n_games = n_games
 
@@ -30,34 +39,45 @@ class Settings:
 
         self.n_parallel_simulations = parralel_simulations
         self.parallel_simulating = parralel_simulations > 1
-        if self.parallel_simulating: self.open_window = False # TODO maybe change
+        if self.parallel_simulating:
+            self.open_window = False  # TODO maybe change
+
 
 class Simulating_settings(Settings):
-    def __init__(self, n_games=100, parralel_simulations=5, print_games_left_every=10) -> None:
-        if parralel_simulations > 20: parralel_simulations = 20 # can't handle to many
-        super().__init__(n_games, 0, parralel_simulations)
+    def __init__(
+        self, n_games=100, parralel_simulations=5, print_games_left_every=10
+    ) -> None:
+        if parralel_simulations > 20:
+            parralel_simulations = 20  # can't handle to many
+        super().__init__(
+            n_games, 0, parralel_simulations, print_individual_results=True
+        )
         self.open_window = False
         self.print_individual_results = False
         self.print_games_left_every = print_games_left_every
-        
+
+
 class Debug_settings(Settings):
-    def __init__(self, n_games=1, wait_time=0.5, print_moves = True) -> None:
+    def __init__(self, n_games=1, wait_time=0.5, print_moves=True) -> None:
         super().__init__(n_games, 0.5, 1)
         self.print_moves = print_moves
         self.wait_time = wait_time
 
+
 # TODO maybe add settings to game
 
-class Main():
+
+class Main:
     def __init__(self, settings):
         self.settings = settings
         self.results = []
         self.n_games_left = settings.n_games
-        self.game = Game() # temporary game for display
+        self.game = Game()  # temporary game for display
         if not self.settings.parallel_simulating:
             self.update_board_qeue = Queue()
             self.user_move_qeue = Queue()
-        if self.settings.open_window: self.open_window()
+        if self.settings.open_window:
+            self.open_window()
 
     def set_players(self, player1, player2):
         self.white_player = player1
@@ -67,41 +87,44 @@ class Main():
         self.drawing_process = Process(target=self.display_loop)
         self.drawing_process.start()
 
-    def make_move(self, game, move): # players can call this to make a move
-        if not game.make_move(move): 
-            return False # move was illegal
-        else: 
-            if self.settings.print_moves: print(move)
-            if self.settings.print_turns: print("white:" if self.game.white_turn else "black:")
+    def make_move(self, game, move):  # players can call this to make a move
+        if not game.make_move(move):
+            return False  # move was illegal
+        else:
+            if self.settings.print_moves:
+                print(move)
+            if self.settings.print_turns:
+                print("white:" if self.game.white_turn else "black:")
 
     def handle_user_move(self):
         if not self.user_move_qeue.empty():
             move = self.user_move_qeue.get_nowait()
-            if not self.white_player.user_move(move): #attempt fails
+            if not self.white_player.user_move(move):  # attempt fails
                 self.black_player.user_move(move)
-    
-    def display_loop(self): # updates the display
+
+    def display_loop(self):  # updates the display
         self.window = Window(self.user_move_qeue)
         while self.window.running:
             if not self.update_board_qeue.empty():
                 updated_board = self.update_board_qeue.get_nowait()
                 self.window.update(updated_board)
-            elif self.update_board_qeue.empty(): 
+            elif self.update_board_qeue.empty():
                 self.window.update()
-    
+
     def start_games_loop(self):
         if self.settings.parallel_simulating:
-            pool = Pool(processes = self.settings.n_parallel_simulations)
+            pool = Pool(processes=self.settings.n_parallel_simulations)
             self.results = pool.map(self.start_new_game, range(self.settings.n_games))
             pool.close()
             pool.join()
         else:
             for i in range(self.settings.n_games):
                 self.results.append(self.start_new_game(i))
-        
+
         # finished
         print("\n")
-        if self.settings.print_raw_results: print(self.results)
+        if self.settings.print_raw_results:
+            print(self.results)
         print(get_results_formatted(self.results))
         self.white_player.close()
         self.black_player.close()
@@ -116,7 +139,7 @@ class Main():
         self.black_player.turn = False
         self.white_player.turn = False
         while not game.is_over:
-            if(game.white_turn):
+            if game.white_turn:
                 self.white_player.request_move(game)
             else:
                 self.black_player.request_move(game)
@@ -128,16 +151,13 @@ class Main():
         if self.settings.print_individual_results:
             print("game outcome: ", game.outcome)
         return game.outcome
-        
+
 
 def main():
-    m = Main(Simulating_settings(n_games=5000, parralel_simulations=20, print_games_left_every=50))
-    m.set_players(
-        AI(Search_settings(depth=2)),
-        AI(Search_settings(depth=2))
-    )
+    m = Main(Debug_settings(n_games=10, wait_time=0.05))
+    m.set_players(AI(Search_settings(depth=2)), AI(Search_settings(depth=2)))
     m.start_games_loop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- v3/search.py
+++ v3/search.py
@@ -1,102 +1,123 @@
 from multiprocessing import Process
 from time import sleep
-from random import shuffle
+
+from itertools import chain
 import chess
 
 from evaluateBoard import evaluate_board, get_material_balance
 
+
 class Search:
-	def __init__(self, settings) -> None:
-		self.settings = settings
-		self.best_move = None
-		self.best_evaluation = -self.settings.matings_score
-		self.board = None
-		self.search_ended = False
-	
-	def end_search(self):
-		self.search_ended = True
-	
-	def start_search(self, board):
-		self.board = board
+    def __init__(self, settings) -> None:
+        self.settings = settings
+        self.best_move = None
+        self.best_evaluation = -self.settings.matings_score
+        self.board = None
+        self.search_ended = False
 
-		if self.settings.iterative_deepening:
-			target_depth = self.settings.depth if self.settings.fixed_depth else 100
-			search_process = Process(target = self.iterative_deepening_search, args = (target_depth))
-			sleep(self.settings.time_limit)
-			self.end_search()
-			search_process.join() # wait until it is finished closing
-		else:
-			self.search(self.settings.depth, -self.settings.matings_score-1, self.settings.matings_score+1)
+    def end_search(self):
+        self.search_ended = True
 
-	
-	def iterative_deepening_search(self, target_depth):
-		for current_depth in range(target_depth):
-			if self.search_ended: break
-			self.search(current_depth)
+    def start_search(self, board):
+        self.board = board
 
-	
-	def search(self, depth, alpha, beta):
-		# time is over
-		# if self.search_ended:
-		# 	return -self.settings.matings_score
-		
-		# check for mate or draw
-		if(self.board.is_checkmate()):
-			return -self.settings.matings_score
-		elif self.board.can_claim_draw() or self.board.is_game_over():
-			return self.settings.draw_score
-		# check for transposition
+        if self.settings.iterative_deepening:
+            target_depth = self.settings.depth if self.settings.fixed_depth else 100
+            search_process = Process(
+                target=self.iterative_deepening_search, args=(target_depth)
+            )
+            sleep(self.settings.time_limit)
+            self.end_search()
+            search_process.join()  # wait until it is finished closing
+        else:
+            self.search(
+                self.settings.depth,
+                -self.settings.matings_score - 1,
+                self.settings.matings_score + 1,
+            )
 
-		if depth == 0:
-			# if self.board.turn: evaluation = get_material_balance(self.board)
-			# else: evaluation = - get_material_balance(self.board)
-			# return evaluation
-			return self.not_quiet_search(alpha, beta)
+    def iterative_deepening_search(self, target_depth):
+        for current_depth in range(target_depth):
+            if self.search_ended:
+                break
+            self.search(current_depth)
 
-		best_move = None
-		best_eval = -self.settings.matings_score
-		moves = list(self.board.legal_moves)
-		shuffle(moves)
+    def search(self, depth, alpha, beta):
+        # time is over
+        # if self.search_ended:
+        # 	return -self.settings.matings_score
 
-		for move in moves:
-			self.board.push(move)
-			# print("start, normal, depth:",depth, "move:", move)
-			evaluation = -self.search(depth-1, -beta, -alpha)
-			# print("end, normal, depth:",depth, "move:", move, "eval:",evaluation)
-			self.board.pop()
-			if evaluation >= beta:
-				# store eval in transposition table
-				return beta # pruned (move was to good)
-			if evaluation > alpha:
-				alpha = evaluation
-				best_move = move
-				best_eval = evaluation
+        # check for mate or draw
+        if self.board.is_checkmate():
+            return -self.settings.matings_score
+        elif self.board.can_claim_draw() or self.board.is_game_over():
+            return self.settings.draw_score
+        # check for transposition
 
-		self.best_move = best_move
-		self.best_evaluation = best_eval
-		# store eval in transposition table
-		return alpha
-		
-	# returns evaluation after having searched checks and captures to reach a quiet position
-	def not_quiet_search(self, alpha, beta, depth=0):
-		if depth>self.settings.capture_search_depth: return beta # TODO come up with a better solution 
-		if self.board.turn: evaluation = evaluate_board(self.board)
-		else: evaluation = - evaluate_board(self.board)
+        if depth == 0:
+            # if self.board.turn: evaluation = get_material_balance(self.board)
+            # else: evaluation = - get_material_balance(self.board)
+            # return evaluation
+            return self.not_quiet_search(alpha, beta)
 
-		if evaluation > beta: return beta # this position won't be reached because it's to good
-		if evaluation > alpha: alpha = evaluation
+        best_move = None
+        best_eval = -self.settings.matings_score
 
-		# generate not quiet moves (captures etc)
-		moves = list(self.board.generate_legal_moves(chess.BB_ALL, self.board.occupied_co[not self.board.turn]))
+        for move in chain(
+            self.board.generate_legal_moves(
+                chess.BB_ALL, self.board.occupied_co[not self.board.turn]
+            ),
+            self.board.generate_legal_moves(chess.BB_ALL, ~self.board.occupied),
+        ):
+            self.board.push(move)
+            # print("start, normal, depth:",depth, "move:", move)
+            evaluation = -self.search(depth - 1, -beta, -alpha)
+            # print("end, normal, depth:",depth, "move:", move, "eval:",evaluation)
+            self.board.pop()
+            if evaluation >= beta:
+                # store eval in transposition table
+                return beta  # pruned (move was to good)
+            if evaluation > alpha:
+                alpha = evaluation
+                best_move = move
+                best_eval = evaluation
 
-		for move in moves:
-			self.board.push(move)
-			# print("start, extended, depth:",depth, "move:", move)
-			evaluation = -self.not_quiet_search(-beta, -alpha, depth+1)
-			# print("end, extended, depth:",depth, "move:", move, "eval:",evaluation)
-			self.board.pop()
+        self.best_move = best_move
+        self.best_evaluation = best_eval
+        # store eval in transposition table
+        return alpha
 
-			if evaluation > beta: return beta
-			if evaluation > alpha: alpha = evaluation
-		
-		return alpha
+    # returns evaluation after having searched checks and captures to reach a quiet position
+    def not_quiet_search(self, alpha, beta, depth=0):
+        if depth > self.settings.capture_search_depth:
+            return beta  # TODO come up with a better solution
+        if self.board.turn:
+            evaluation = evaluate_board(self.board)
+        else:
+            evaluation = -evaluate_board(self.board)
+
+        if evaluation > beta:
+            return beta  # this position won't be reached because it's to good
+        if evaluation > alpha:
+            alpha = evaluation
+
+        # generate not quiet moves (captures etc)
+        moves = list(
+            self.board.generate_legal_moves(
+                chess.BB_ALL, self.board.occupied_co[not self.board.turn]
+            )
+        )
+
+        for move in moves:
+            self.board.push(move)
+            # print("start, extended, depth:",depth, "move:", move)
+            evaluation = -self.not_quiet_search(-beta, -alpha, depth + 1)
+            # print("end, extended, depth:",depth, "move:", move, "eval:",evaluation)
+            self.board.pop()
+
+            if evaluation > beta:
+                return beta
+            if evaluation > alpha:
+                alpha = evaluation
+
+        return alpha
